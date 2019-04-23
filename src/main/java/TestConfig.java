515d5893ecc79a9c253ebcd717e81fda0d310b56
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2019-04-23 17:17:15 +0530

    alpha

---
+++ src/main/java/TestConfig.java
@@ -0,0 +1,34 @@
+import io.mosip.registration.audit.AuditFactory;
+import io.mosip.registration.dao.AppAuthenticationDAO;
+import io.mosip.registration.dao.RegistrationCenterDAO;
+import io.mosip.registration.dao.ScreenAuthorizationDAO;
+import io.mosip.registration.dao.UserDetailDAO;
+import io.mosip.registration.service.LoginService;
+import io.mosip.registration.service.impl.LoginServiceImpl;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+//    @Mock
+//    public AppAuthenticationDAO appAuthenticationDAO;
+//
+//    @Mock
+//    public UserDetailDAO userDetailDAO;
+//
+//    @Mock
+//    public RegistrationCenterDAO registrationCenterDAO;
+//
+//    @Mock
+//    public AuditFactory auditFactory;
+//
+//    @Mock
+//    public ScreenAuthorizationDAO screenAuthorizationDAO;
+//
+//    @Bean
+//    LoginService loginService(){
+//        return new LoginServiceImpl();
+//    }
+}
